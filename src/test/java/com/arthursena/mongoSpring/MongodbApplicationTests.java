f3e06fba50434bc3e8241b0063b89205ab62767f
Arthur Sena <sena.arthur698@gmail.com>
2024-08-21 10:35:02 -0300

    Primeiro commit - Projeto criado

---
+++ src/test/java/com/arthursena/mongoSpring/MongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthursena.mongoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
