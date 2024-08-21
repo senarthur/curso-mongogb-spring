f444f324eab73a99886d263922a9eb0ef036c8b4
Arthur Sena <sena.arthur698@gmail.com>
2024-08-21 13:42:16 -0300

    Obtendo um usuário por id

--- src/main/java/com/arthursena/mongoSpring/services/UserService.java
+++ src/main/java/com/arthursena/mongoSpring/services/UserService.java
@@ -1,12 +1,14 @@
 package com.arthursena.mongoSpring.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.arthursena.mongoSpring.domain.User;
 import com.arthursena.mongoSpring.repository.UserRepository;
+import com.arthursena.mongoSpring.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
 	}
 }
