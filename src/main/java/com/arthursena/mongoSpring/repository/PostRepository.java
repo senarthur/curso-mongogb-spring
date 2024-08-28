e78773fb81c0afbad492ee756270e62980778cac
Arthur Sena <sena.arthur698@gmail.com>
2024-08-28 13:45:51 -0300

    Consulta simples com query methods

--- src/main/java/com/arthursena/mongoSpring/repository/PostRepository.java
+++ src/main/java/com/arthursena/mongoSpring/repository/PostRepository.java
@@ -1,9 +1,12 @@
 package com.arthursena.mongoSpring.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.arthursena.mongoSpring.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/arthursena/mongoSpring/services/PostService.java
+++ src/main/java/com/arthursena/mongoSpring/services/PostService.java
@@ -1,5 +1,6 @@
 package com.arthursena.mongoSpring.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,9 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> post = repo.findById(id);
 		return post.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
+	}
+	
+	public List<Post> findByTitle(String title) {
+		return repo.findByTitleContainingIgnoreCase(title);
 	}
 }
