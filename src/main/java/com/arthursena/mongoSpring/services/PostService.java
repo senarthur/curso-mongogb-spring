442c1334c4c1ba29c891a8936dd19f58a8b6961a
Arthur Sena <sena.arthur698@gmail.com>
2024-08-28 12:14:15 -0300

    Obtendo post por id

---
+++ src/main/java/com/arthursena/mongoSpring/services/PostService.java
@@ -0,0 +1,22 @@
+package com.arthursena.mongoSpring.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.arthursena.mongoSpring.domain.Post;
+import com.arthursena.mongoSpring.repository.PostRepository;
+import com.arthursena.mongoSpring.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> post = repo.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
+	}
+}
